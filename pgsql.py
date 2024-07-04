897494f748bc67b2844218707d182daa10a0e13c
giladrv <giladrv@gmail.com>
2024-07-04 14:48:54 +0300

    con args

--- pgsql.py
+++ pgsql.py
@@ -10,6 +10,12 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.extras import RealDictCursor, execute_batch, execute_values
 from psycopg2.sql import Identifier as PgI, Literal as PgL, SQL as PgQ
 
+def do_iam_auth(con_args: Dict[str, Any]):
+    if con_args['password'] == 'IAM':
+        from awspy.rds import RDS
+        token = RDS().iam_auth(con_args['database'], con_args['port'], con_args['user'])
+        con_args['password'] = token
+
 def read_con_args_from_env(env: str):
     con_args = {
         'host':     os.environ[f'{env}_DB_HOST'],
@@ -42,13 +48,10 @@ class PgSQL():
     def __init__(self, con_args: Dict[str, Any] = None, qdir: str = 'sql', env: str = None):
         if env is not None:
             con_args = read_con_args_from_env(env)
-        if con_args['password'] == 'IAM':
-            from awspy.rds import RDS
-            con_args['password'] = RDS().iam_auth(con_args['database'], con_args['port'], con_args['user'])
-        self._con_args = con_args
-        self._con_last = datetime.now()
-        self._con = None
-        self._qdir = qdir
+        self.con = None
+        self.con_args = con_args
+        self.con_last = datetime.now()
+        self.qdir = qdir
         # Catch unhandled exceptions and close the connection
         next_excepthook = sys.excepthook
         def close_on_exception(etype, value, tb):
@@ -57,9 +60,10 @@ class PgSQL():
             next_excepthook(etype, value, tb)
         sys.excepthook = close_on_exception
 
-    def _connect(self, con_args: Dict[str, Any] = None):
+    def connect(self, con_args: Dict[str, Any] = None):
         if con_args is None:
-            con_args = self._con_args
+            con_args = self.con_args.copy()
+        do_iam_auth(con_args)
         return psycopg2.connect(**con_args)
 
     def _exec(self, fetch: Fetch, qname: str, qvars: Dict[str, Any] = None, query: str = None):
@@ -77,19 +81,19 @@ class PgSQL():
         return res
 
     def close(self):
-        if self._con is None:
+        if self.con is None:
             return
-        if self._con.closed == 0:
-            self._con.close()
-        self._con = None
+        if self.con.closed == 0:
+            self.con.close()
+        self.con = None
 
     def connection(self):
-        if self._con is None \
-                or self._con.closed != 0 \
-                or (datetime.now() - self._con_last).total_seconds() > 60:
-            self._con = self._connect()
-            self._con_last = datetime.now()
-        return self._con
+        if self.con is None \
+                or self.con.closed != 0 \
+                or (datetime.now() - self.con_last).total_seconds() > 60:
+            self.con = self.connect()
+            self.con_last = datetime.now()
+        return self.con
 
     def exec(self, qname: str, qvars: Dict[str, Any] = None, query: str = None):
         return self._exec(Fetch.Zro, qname, qvars = qvars, query = query)
@@ -118,7 +122,7 @@ class PgSQL():
         if qname[0] in './':
             sql_file = qname
         else:
-            sql_file = os.path.join(self._qdir, f'{qname}.sql')
+            sql_file = os.path.join(self.qdir, f'{qname}.sql')
         if not os.path.exists(sql_file):
             raise Exception(f'SQL `{qname}` not defined!')
         with open(sql_file) as f:
